97e9504557a959af304a629148cc81ed83063cd8
Roi Glinik <groi.tech@gmail.com>
2019-08-17 10:29:53 +0300

    wip text 23
    another minor fixes for words

--- suffix.php
+++ suffix.php
@@ -1,20 +1,21 @@
 <?php
 
 	$suffix_patterns = array(
-	"/e({$C}(ut|on|aym|iyut|it|iyyut|ot))\\b/u",
+	"/\\b(($C|$V){2,})e({$C}(ut|on|aym|iyut|it|iyyut|ot))\\b/u",
 
 	"/((($C|$V){2,})ayim)/u", //9
 	"/\\b((($C|$V)+)ehem)\\b/u", //10
 	"/\\b((($C|$V)+)ai)\\b/u", //11
 
 	"/\\b(($C|$V)+[e]{$C})et\\b/u",
-	"/(\\b(\\S*)[a][c][t])\\b/u",
-	"/(\\b(\\S{4,})[y][i][m])\\b/u",
+	"/(\\b(\\S*)act)\\b/u",
+	"/(\\b(\\S{4,})yim)\\b/u",
+	"/\\b(($C|$V){2,})({$V})aˁ\\b/u", //15
 	);
 
 	//print_r($suffix_patterns);
 	$suffix_patterns_rep = array(
-	'\1',
+	'\1\3',
 
 	'\2aym',//9
 	'\2eihem',//10
@@ -23,12 +24,14 @@
 	'\1t',
 	'\2ect',
 	'\2ym',
+	'\1\3ˁ'
 	);
 
 	$exceptions = array(
 		"heççegim",
 		'šˀelot',
 		'ˀemet',
+		'gderot'
 	);
 
 	$size = count($suffix_patterns);
